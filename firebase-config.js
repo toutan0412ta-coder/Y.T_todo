e59c2da6fda18df2c4abda3bd898445c7e297d95
toutan0412ta-coder <toutan0412ta@gmail.com>
2026-08-28 01:05:51 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+/* firebase-config.js  ── 管理者画面・こども画面 共通 */
+window.LQ_FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyDUOcv2CJ2ttsHYSvY88YjWKnN0zq7gHMQ",
+  authDomain:        "yt-todo-c0a6f.firebaseapp.com",
+  databaseURL:       "https://yt-todo-c0a6f-default-rtdb.firebaseio.com/",
+  projectId:         "yt-todo-c0a6f",
+  storageBucket:     "yt-todo-c0a6f.firebasestorage.app",
+  messagingSenderId: "667982676392",
+  appId:             "1:667982676392:web:3d1ed200a8c2c742565d9d"
+};
+window.LQ_ROOT     = "lifequest";  /* DBのルート名（変えなくてOK） */
+window.LQ_ADMIN_PIN = "";          /* 例:"1234" と入れると管理者画面に暗証番号がかかります */
